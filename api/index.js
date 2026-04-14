822c1f13ab686324368008015d5e530c2b278af8
Shaikh Amir Rukmoddin <amirshaikh320340@gmail.com>
2026-04-15 00:53:29 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         "Authorization": `Bearer ${process.env.GROQ_API_KEY_1}`
       },
       body: JSON.stringify({
-        model: "llama-3.1-8b-insant",
+        model: "llama-3.1-8b-instant",
         messages: [{ role: "user", content: message }]
       })
     });
