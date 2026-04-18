37ff1cee913fdec201e1b3593e9433d1d31f26a6
Shaikh Amir Rukmoddin <amirshaikh320340@gmail.com>
2026-04-18 23:18:25 +0530

    Create adzunaService.js

---
+++ jobs/adzunaService.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const { DOMAINS } = require("./config");
+
+const APP_ID = "YOUR_APP_ID";
+const APP_KEY = "YOUR_APP_KEY";
+
+function buildQuery() {
+  return DOMAINS.join(" OR ");
+}
+
+async function fetchJobs(city, page = 1) {
+  const res = await axios.get(
+    `https://api.adzuna.com/v1/api/jobs/in/search/${page}`,
+    {
+      params: {
+        app_id: APP_ID,
+        app_key: APP_KEY,
+        results_per_page: 50,
+        what: buildQuery(),
+        where: city
+      }
+    }
+  );
+
+  return res.data.results;
+}
+
+module.exports = { fetchJobs };
